c0c69dcf061d877c2c8768d502254f20962ddf32
Amilcar Rey <amilcar.rey@aces.com.ar>
2020-02-14 18:06:41 -0300

    Adding footer

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
-  <div id="app" class="container">
-    <img alt="Vue logo" src="./assets/logo.png" />    
-    <Quotes />
+  <div id="app" class="has-background-light">
+    <section class="hero is-fullheight is-desktop">
+      <div class="hero-body">
+        <div class="container">
+        
+          <Quotes  />
+        
+      </div>
+      
+      </div>
+      <Footer/>
+    </section>
   </div>
 </template>
 
 <script>
 import Quotes from "./components/Quotes.vue";
+import Footer from "./components/Footer.vue";
 
 export default {
   name: "app",
   components: {
-    Quotes
+    Quotes,
+    Footer
   }
 };
 </script>
 
 <style>
 #app {
-  font-family: "Montserrat", Helvetica, sans-serif;
+  font-family: "Springsteel Serif", Helvetica, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #666666;
-  margin-top: 60px;
+  height: 100%;
 }
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,31 @@
+<template>
+  <footer class="footer is-clipped	">
+    <div class="content has-text-centered">
+      <p>
+        <a href="https://www.linkedin.com/in/amilcarrey/" target="blank"
+          >by amilcarrey.</a
+        >
+      </p>
+    </div>
+  </footer>
+</template>
+<script>
+export default {
+  name: "Footer"
+};
+</script>
+<style scoped>
+.footer {
+  background-color: rgb(10, 10, 10);
+  padding: 0.2rem 0rem 0.2rem;
+  color: rgb(197, 197, 197);
+  text-align: center;
+}
+.footer a {
+  color: rgb(197, 197, 197);
+}
+.footer p {
+  display: inline-block;
+  vertical-align: middle;
+}
+</style>
